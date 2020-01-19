4aad56ac7fa7ae56f2ddc5e682b5d29ce3e89a4e
milen <milen@192.168.1.40>
2020-01-19 17:38:22 +0100

    kata 5 version 1

---
+++ src/kata5/Kata5.java
@@ -0,0 +1,37 @@
+package kata5;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Kata5 {
+
+    public static void main(String[] args) {
+      
+        connect();
+        SelectApp app = new SelectApp();
+        app.selectAll();
+
+    } 
+    
+    private static void connect() {
+        Connection conn = null;
+        try {
+            // parámetros de la BD
+            String url= "jdbc:sqlite:Kata5.db";
+            // creamos una conexión a la BD
+            conn= DriverManager.getConnection(url);
+            System.out.println("Connection estableshed");
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            } finally {
+                try {
+                    if(conn!= null) {
+                    conn.close();
+                    }
+                } catch (SQLException ex) {
+                        System.out.println(ex.getMessage());
+                  }
+              }
+    }
+}
